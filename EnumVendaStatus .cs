f7fadaeba7b71543d77949ab47e88486cf1a6c56
EDIMIR LUIZ <76841015+luizk9@users.noreply.github.com>
2022-10-31 13:22:21 -0300

    Models
    pasta Models da api_tech

---
+++ EnumVendaStatus .cs
@@ -0,0 +1,12 @@
+namespace api_tech.Models
+{
+    public enum EnumVendaStatus  
+    {
+        Aguardando_Pagamento=1,
+        Pagamento_Realizado=2,
+        Cancelado_Pagmento=3,
+        Envio_Transportadora=4,
+        Produto_entregue=5
+        
+    }
+}
---
+++ Produtot.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_tech.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Item_Produto { get; set; }
+        public string Nome_Produto { get; set; }
+        
+    }
+}
